589b0530fc5cae1cac352b74d5810cb71357f666
Sevan-s <sevan.sarikaya@gmail.com>
2025-06-30 12:07:03 +0200

    [ADD] associateProduct

--- src/models/product.js
+++ src/models/product.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    name: { type: String, required: true},
+    name: { type: String, required: true },
     price: { type: Number, required: true },
     description: { type: String, required: true },
     shortDescription: { type: String, required: true },
     maintenance: { type: String, required: true },
     stock: { type: Number, default: 0 },
-    category: { type: String},
+    category: { type: String },
     imageUrls: { type: [String], default: [] },
     createdAt: { type: Date, default: Date.now },
-    options:{type: [String], default: []}
+    options: { type: [String], default: [] },
+    associateProduct: { type: String, default: "" }
 });
 
 module.exports = mongoose.model('Product', productSchema);
