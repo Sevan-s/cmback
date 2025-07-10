f63941940634a6ea35fbc6eccdef94b39430aa77
Sevan-s <sevan.sarikaya@gmail.com>
2025-07-10 14:16:03 +0200

    [ADD] fabricsOptions

--- src/conrtollers/productController.js
+++ src/conrtollers/productController.js
@@ -2,13 +2,13 @@ const Product = require('../models/product');
 
 exports.CreateProduct = async (req, res) => {
     try {
-        const { name, price, description, shortDescription, maintenance, stock, category, subCategory, imageUrls, options, associateProduct } = req.body;
+        const { name, price, description, shortDescription, maintenance, stock, category, subCategory, imageUrls, options, fabrtics, associateProduct } = req.body;
         const existingProduct = await Product.findOne({ name });
 
         if (existingProduct) {
             return res.status(400).json({ error: 'product name already exist' });
         }
-        const newProduct = new Product({ name, price, description, shortDescription, maintenance, stock, category, subCategory, imageUrls, options, associateProduct });
+        const newProduct = new Product({ name, price, description, shortDescription, maintenance, stock, category, subCategory, imageUrls, options, fabrtics, associateProduct });
         await newProduct.save();
         res.status(201).json({ Product: newProduct })
     } catch (error) {
@@ -54,7 +54,7 @@ exports.delProductById = async (req, res) => {
 
 exports.putProductById = async (req, res) => {
     try {
-        const { name, price, description, shortDescription, maintenance, stock, category, subCategory, imageUrls, options, associateProduct } = req.body;
+        const { name, price, description, shortDescription, maintenance, stock, category, subCategory, imageUrls, options, fabrtics, associateProduct } = req.body;
         const productId = req.params.id;
 
         if (!productId || !name || !price) {
@@ -71,6 +71,7 @@ exports.putProductById = async (req, res) => {
             category,
             subCategory,
             options,
+            fabrtics,
             associateProduct,
         };
 
