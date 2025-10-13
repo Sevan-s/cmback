5e6182e24511cb3196472a491f4787e7c3b3fa24
Sevan-s <sevan.sarikaya@gmail.com>
2025-10-13 15:42:15 +0200

    [FIX] who schema

--- src/models/product.js
+++ src/models/product.js
@@ -7,26 +7,32 @@ const dimensionSchema = new mongoose.Schema(
   },
   { _id: true }
 );
+
+const WhoSchema = new mongoose.Schema({
+  name: { type: String, required: true, trim: true },
+  price: { type: Number, required: true, min: 0 },
+}, { _id: false });
+
 const productSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    price: { type: Number, required: true },
-    description: { type: String, required: true },
-    dimension: { type: String, required: true },
-    composition: { type: String, required: true },
-    shortDescription: { type: String, required: true },
-    maintenance: { type: String, required: true },
-    stock: { type: Number, default: 0 },
-    category: { type: String },
-    subCategory: { type: [String] },
-    imageUrls: { type: [String], default: [] },
-    createdAt: { type: Date, default: Date.now },
-    options: { type: [String], default: [] },
-    fabrics: { type: [String], default: [] },
-    fabricsQuantities: { type: Number, default: 1 },
-    associateProduct: { type: String, default: "" },
-    who: { type: [String], default: [] },
-    lot: { type: [{ quantities: Number, price: Number }], default: [] },
-    dimensions: { type: [dimensionSchema], default: [] },
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  description: { type: String, required: true },
+  dimension: { type: String, required: true },
+  composition: { type: String, required: true },
+  shortDescription: { type: String, required: true },
+  maintenance: { type: String, required: true },
+  stock: { type: Number, default: 0 },
+  category: { type: String },
+  subCategory: { type: [String] },
+  imageUrls: { type: [String], default: [] },
+  createdAt: { type: Date, default: Date.now },
+  options: { type: [String], default: [] },
+  fabrics: { type: [String], default: [] },
+  fabricsQuantities: { type: Number, default: 1 },
+  associateProduct: { type: String, default: "" },
+  who: { type: [WhoSchema], default: [] },
+  lot: { type: [{ quantities: Number, price: Number }], default: [] },
+  dimensions: { type: [dimensionSchema], default: [] },
 });
 
 module.exports = mongoose.model('Product', productSchema);
